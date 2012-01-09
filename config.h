6b86b223c3acb79f0d7a1bf92e4439626939564e
LemonBoy <segnalazionidalweb@gmail.com>
2012-01-09 21:09:48 +0100

    Screw you.

---
+++ config.h
@@ -0,0 +1,11 @@
+/* This is the default config, should work fine
+ * for all the users, but in case it doesnt for
+ * you just change the values below.
+ * Uid can be obtained by
+ *      id -u <your user name>
+ * Gid can be obtained by
+ *      id -g <your user name>
+ */
+
+#define CONFIG_USER_UID  1000
+#define CONFIG_USER_GID  100
